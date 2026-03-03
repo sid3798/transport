81db2c713ee98fe906459f97adc501c05cafb67b
Siddhesh <sidgadge1191@gmail.com>
2026-03-04 01:46:26 +0530

    swith to core rqe

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const cors = require("cors");
 
 const app = express();
@@ -10,6 +10,7 @@ app.post("/generate-pdf", async (req, res) => {
   const invoiceData = req.body;
 
   const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/chromium-browser",
     args:["--no-sandbox","--disable-setuid-sandbox"],
     headless:"new"
   });
