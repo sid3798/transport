8f28a40034f5fa6b89f8687b121311c37d5d5736
Siddhesh <sidgadge1191@gmail.com>
2026-03-04 01:50:21 +0530

    sriptchnged

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.post("/generate-pdf", async (req, res) => {
   const browser = await puppeteer.launch({
     executablePath: "/usr/bin/chromium-browser",
     args:["--no-sandbox","--disable-setuid-sandbox"],
-    headless:"new"
+    headless:"true"
   });
 
   const page = await browser.newPage();
