3e9c286ace9880633d2ed384300e32d183e1aa16
Siddhesh <sidgadge1191@gmail.com>
2026-03-08 13:51:52 +0530

    use env variable for google drive token

--- backend/config/driveAuth.js
+++ backend/config/driveAuth.js
@@ -1,12 +1,8 @@
-const fs = require("fs");
-const path = require("path");
 const { google } = require("googleapis");
-
-const TOKEN_PATH = path.join(__dirname, "token.json");
 
 async function authorize() {
 
-  const token = JSON.parse(fs.readFileSync(TOKEN_PATH));
+  const token = JSON.parse(process.env.GOOGLE_TOKEN);
 
   const oAuth2Client = new google.auth.OAuth2();
 
