cbc4d33b74cbbe1e6fb48e5e583f32fb6b1470ed
Siddhesh <sidgadge1191@gmail.com>
2026-03-08 14:10:58 +0530

    fix google oauth refresh

--- backend/config/driveAuth.js
+++ backend/config/driveAuth.js
@@ -4,7 +4,10 @@ async function authorize() {
 
   const token = JSON.parse(process.env.GOOGLE_TOKEN);
 
-  const oAuth2Client = new google.auth.OAuth2();
+  const oAuth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET
+  );
 
   oAuth2Client.setCredentials(token);
 
